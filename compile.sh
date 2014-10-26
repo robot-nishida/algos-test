06a90e6d1082c7758e982d5086f9ec6df57cb402
Ryosuke, Nishida <nishida@rurobots.info>
2014-10-27 01:17:19 +0900

    環境を整えた

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "Ready...OK."
-g++ -w -o exe_algos main.cpp -I ~/Software/local/include/ `ode-config --cflags --libs` -ldrawstuff -framework GLUT -framework OpenGL || { echo oops!; exit 1; }
+g++ -Wall -o exe_algos main.cpp -I ~/Software/local/include/ `ode-config --cflags --libs` -ldrawstuff -framework GLUT -framework OpenGL || { echo oops!; exit 1; }
 echo "Done...GO.\n>>> - - - "
 ./exe_algos
